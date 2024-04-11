8b1ae208e2d3d3eedff31e8c33bfe94f59d3aed2
Duoc Vo <duoc.vo@kyanon.digital>
2024-04-11 23:16:46 +0700

    Init

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Detect device mobile
+  var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  var backgroundContainer = document.getElementById('backgroundContainer');
+
+  if (isMobile) {
+    backgroundContainer.style.backgroundImage = 'url(./assets/images/image-background.png)';
+  } else {
+    // When device isn't mobile
+    var warningIcon = document.createElement('img');
+    warningIcon.src = 'warning-icon.png';
+    backgroundContainer.appendChild(warningIcon);
+  }
+
+  var language = navigator.language || navigator.userLanguage;
+
+  if (language.toLowerCase().startsWith('en')) {
+    document.getElementById('btnInstall').setAttribute('title', 'Install app');
+  }
+});
+
+document.getElementById('btnInstall').addEventListener('click', function () {
+  var platform = getPlatform();
+
+  if (platform === 'ios') {
+    window.location.href = "https://apps.apple.com/app/apple-store/id6473516617";
+  } else if (platform === 'android') {
+    window.location.href = "https://play.google.com/store/apps/details?id=vn.com.aeon.kidsclub";
+  } else {
+    alert("Platform không được hỗ trợ!");
+  }
+});
+
+function getPlatform() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return 'ios';
+  }
+
+  else if (/android/i.test(userAgent)) {
+    return 'android';
+  }
+
+  else {
+    return null;
+  }
+}
