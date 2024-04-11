fe6127cd08aec072ca0cf9c0d4c9a3a8c4a26509
Duoc Vo <duoc.vo@kyanon.digital>
2024-04-12 01:17:35 +0700

    Init

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
   var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
   var backgroundContainer = document.getElementById("backgroundContainer");
 
-    backgroundContainer.style.backgroundImage =
-      "url(./assets/images/image-background.png)";
- 
+  backgroundContainer.style.backgroundImage =
+    "url(./assets/images/image-background.png)";
 
   var language = navigator.language || navigator.userLanguage;
 
@@ -18,22 +17,15 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = "kidsclubdev://homeee";
   } else if (platform === "android") {
     // window.open("intent://homeee#Intent;package=com.kd.aeonkids.beta;scheme=kidsclubdev;end;");
-    // document.getElementById("alo").click();
+    document.getElementById("alo").click();
   }
-
-
-
 });
 
 document.getElementById("btnOpen").addEventListener("click", function () {
   var platform = getPlatform();
 
-  // window.open(
-  //   "intent://homeee#Intent;package=com.kd.aeonkids.beta;scheme=kidsclubdev;end;"
-  // );
-
   if (platform === "ios") {
-      window.location.href = "kidsclubdev://homeee";
+    window.location.href = "kidsclubdev://homeee";
   } else if (platform === "android") {
     document.getElementById("alo").click();
   } else {
@@ -44,22 +36,20 @@ document.getElementById("btnOpen").addEventListener("click", function () {
 document.getElementById("btnInstall").addEventListener("click", function () {
   var platform = getPlatform();
 
-  document.getElementById("alo").click();
+  window.open(
+    "intent://homeee#Intent;package=com.kd.aeonkids.beta;scheme=kidsclubdev;end;"
+  );
 
-
-  // window.open(
-  //   "intent://homeee#Intent;package=com.kd.aeonkids.beta;scheme=kidsclubdev;end;"
-  // );
-
-  // if (platform === "ios") {
-  //   window.location.href =
-  //     "https://apps.apple.com/app/apple-store/id6473516617";
-  // } else if (platform === "android") {
-  //   alert("Android");
-  //   // window.location.href("intent://homeee#Intent;package=com.kd.aeonkids.beta;scheme=kidsclubdev;end;");
-  // } else {
-  //   alert("Platform không được hỗ trợ!");
-  // }
+  if (platform === "ios") {
+    window.location.href =
+      "https://apps.apple.com/app/apple-store/id6473516617";
+  } else if (platform === "android") {
+    alert("Android");
+    window.location.href =
+      "https://play.google.com/store/apps/details?id=vn.com.aeon.kidsclub";
+  } else {
+    alert("Platform không được hỗ trợ!");
+  }
 });
 
 function getPlatform() {
